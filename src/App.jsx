546db5c69ddca1e74979462d2b216547c6b0b37d
giasbd67-tech <giasbd67@gmail.com>
2026-08-08 19:52:32 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2465,7 +2465,7 @@ const downloadDonorCertificate = (donor) => {
 
 
 {(isAdmin || isUnlocked) && (
-  <div className="flex items-center gap-1">
+  <div className="flex flex-col justify-center gap-1">
 
     {/* আইডি কার্ড বাটন - সুপার স্লিম */}
     <button onClick={() => downloadVolunteerCard(v)} title="কার্ড" className="flex items-center gap-0.5 px-1.5 py-0.5 bg-white border rounded-sm text-slate-600 hover:bg-slate-50 shadow-none text-[8px] font-bold transition-all border-slate-200">
